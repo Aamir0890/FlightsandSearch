1ee011d06f956169673b3868af6e470ae16dace0
Yourdad404 <arshadaamir09@gmail.com>
2023-07-27 16:58:50 +0530

    setting up db models and inserting city

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express=require("express")
 const bodyParser=require("body-parser")
 const {PORT}=require("./config/serverConfig")
-
+const CityRepository =require('./repository/city-repository')
 
 const setupAndStartServer=async()=>{
        const app=express();
        app.use(bodyParser.json());
        app.use(bodyParser.urlencoded({extended:true}))
 
-    
+       
+
        app.listen(3000,()=>{
         console.log(`Server started at PORT ${PORT}`)
+        const repo=new CityRepository();
+
+        repo.creatCity({name:"New Delhi"})
        })
 }
 
