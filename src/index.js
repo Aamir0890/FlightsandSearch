57c769afd59c96fac6f7f45b6572e0c1ab05ccc6
Yourdad404 <arshadaamir09@gmail.com>
2023-07-27 17:03:25 +0530

    added unique constarints and started cityrepository

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const setupAndStartServer=async()=>{
        app.use(bodyParser.json());
        app.use(bodyParser.urlencoded({extended:true}))
 
-       
+
 
        app.listen(3000,()=>{
         console.log(`Server started at PORT ${PORT}`)
         const repo=new CityRepository();
 
-        repo.creatCity({name:"New Delhi"})
+        
        })
 }
 
